770dd82e2a18c58baf408fda9fff6779e877b32e
Mariamfathi17 <86616263+Mariamfathi17@users.noreply.github.com>
2024-08-27 00:17:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,4 @@ with col1:
     pregnancies = st.number_input("Number of Pregnancies", min_value=0, max_value=20, step=1, value=1)
     glucose = st.number_input("Glucose Level", min_value=0, max_value=200, step=1, value=100)
     blood_pressure = st.number_input("Blood Pressure (mm Hg)", min_value=0, max_value=122, step=1, value=70)
-    skin_thickness = st.number_input("Skin Thickness (mm)", min_value=0,
+    skin_thickness = st.number_input("Skin Thickness (mm)"), min_value=0,
